bcb9f96070b68edc30ecb57422cda73da65b4b43
Santii05-C <santi.c.pgm@gmail.com>
2025-02-16 22:55:18 -0300

    last commit

---
+++ backend/routes.py
@@ -0,0 +1,12 @@
+from app import app, db
+from flask import request, jsonify
+from models import Friend
+
+@app.route("/api/friends", methods=["GET"])
+def get_friend():
+    friends = Friend.query.all()
+    result = [friend.to_json() for friend in friends]
+    return jsonify(result)
+
+
+#26:57
