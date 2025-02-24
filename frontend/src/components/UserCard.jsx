88d61f8e7014b64dee8a80b9e98664a61b4147c1
Santii05-C <santi.c.pgm@gmail.com>
2025-02-23 22:39:19 -0300

    Realicé la creación del componte UserCard.jsx pero no está terminado

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,5 +1,50 @@
-const UserCard = () => {
-  return <div>UserCard</div>;
+import {
+  Avatar,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { BiTrash } from "react-icons/bi";
+
+const UserCard = ({ user }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <Flex gap={4}>
+          <Flex flex={"1"} gap={"4"} alignItems={"center"}>
+            <Avatar src="https://avatar.iran.liara.run/public" />
+
+            <Box>
+              <Heading size="sm">{user.name}</Heading>
+              <Text>{user.role}</Text>
+            </Box>
+          </Flex>
+
+          <Flex>
+            {/* <EditModal user={user} setUsers={setUsers} /> */}
+            <IconButton
+              variant="ghost"
+              colorScheme="red"
+              size={"sm"}
+              aria-label="See menu"
+              icon={<BiTrash size={20} />}
+              //   onClick={handleDeleteUser}
+            />
+          </Flex>
+        </Flex>
+      </CardHeader>
+
+      <CardBody>
+        <Text>{user.description}</Text>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default UserCard;
--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -1,5 +1,24 @@
+import { Grid } from "@chakra-ui/react";
+import { USERS } from "../dummy/dummy";
+import UserCard from "./UserCard";
+
 const UserGrid = () => {
-  return <div>UserGrid</div>;
+  return (
+    <>
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+        }}
+        gap={4}
+      >
+        {USERS.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default UserGrid;
