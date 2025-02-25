98629be115edb14ce8c82976a4977c3f2e65f32d
Santii05-C <santi.c.pgm@gmail.com>
2025-02-24 23:30:23 -0300

    Finalicé algunos cambios en el backend para hacer el deploy

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+# Server static files from the "dist" folder under the "frontend" directory
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
+# api routes
 import routes
 
 with app.app_context():
