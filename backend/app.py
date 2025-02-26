9371b56b86c83e4faead515447d6ecf2ee1f358e
Santii05-C <santi.c.pgm@gmail.com>
2025-02-26 16:50:35 -0300

    last commit

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,34 @@
-# # TODO: UPDATE THIS FILE FOR DEPLOYMENT
-# from flask import Flask, send_from_directory
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
-# import os
+# TODO: UPDATE THIS FILE FOR DEPLOYMENT
+from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
-# CORS(app) 
+CORS(app) 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# frontend_folder = os.path.join(os.getcwd(),"..","frontend")
-# dist_folder = os.path.join(frontend_folder,"dist")
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
 
-# @app.route("/",defaults={"filename":""})
-# @app.route("/<path:filename>")
-# def index(filename):
-#   if not filename:
-#     filename = "index.html"
-#   return send_from_directory(dist_folder,filename)
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
 
-# # api routes
-# import routes
+# api routes
+import routes
 
-# with app.app_context():
-#   db.create_all()
+with app.app_context():
+  db.create_all()
 
-# if __name__ == "__main__":
-#   app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
